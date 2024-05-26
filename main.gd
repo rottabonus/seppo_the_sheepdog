c7f6b4f1c8b889249354eb63765fbd73f8705a07
Felix Hallenberg <feqwin@gmail.com>
2024-05-26 19:19:46 +0300

    Add SeppoDub

--- main.gd
+++ main.gd
@@ -3,8 +3,10 @@ extends Node2D
 var master_caller: AudioStreamPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AudioStreamPlayer.play()
 	DialogueManager.show_example_dialogue_balloon(load("res://dialogue/first.dialogue"), "start")
 	master_caller = AudioStreamPlayer.new()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Globals.has_player_talked_to_master == true:
@@ -24,3 +26,7 @@ func _process(_delta):
 #
 #func _on_call_loop_timeout():
 	#call_seppo()
+
+
+func _on_audio_stream_player_finished():
+	$AudioStreamPlayer.play()
