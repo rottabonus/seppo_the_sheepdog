dbb5aa8071d4afdf87207c44c5cf6b95c78f55b1
Felix Hallenberg <feqwin@gmail.com>
2024-05-27 19:19:37 +0300

    Add barn and tractor
    - though the quality of details is not quite there
    - at least you can see the burnout of this project x)

--- main.gd
+++ main.gd
@@ -2,31 +2,33 @@ extends Node2D
 
 var master_caller: AudioStreamPlayer
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-	#$AudioStreamPlayer.play()
-	#DialogueManager.show_example_dialogue_balloon(load("res://dialogue/first.dialogue"), "start")
-	#master_caller = AudioStreamPlayer.new()
-	#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(_delta):
-	#if Globals.has_player_talked_to_master == true:
-		#master_caller.stop()
+func _ready():
+	$MainMusic.play()
+	DialogueManager.show_example_dialogue_balloon(load("res://dialogue/first.dialogue"), "start")
+	master_caller = AudioStreamPlayer.new()
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if Globals.has_player_talked_to_master == true:
+		master_caller.stop()
 
-#func call_seppo():
-	#var audio_stream_array = [load("res://sound/seppo1.wav"), load("res://sound/seppo2.wav"), load("res://sound/seppo3.wav")]
-	#randomize()
-	#var clip_to_play = audio_stream_array[randi() % audio_stream_array.size()]
-	#master_caller.set_stream(clip_to_play)
-	#master_caller.play()
-#
-#func _on_master_call_timer_timeout():
-	#call_seppo()
-	#$CallLoop.start()
-#
-#
-#func _on_call_loop_timeout():
-	#call_seppo()
+func call_seppo():
+	var audio_stream_array = [load("res://sound/seppo1.wav"), load("res://sound/seppo2.wav"), load("res://sound/seppo3.wav")]
+	randomize()
+	var clip_to_play = audio_stream_array[randi() % audio_stream_array.size()]
+	master_caller.set_stream(clip_to_play)
+	master_caller.play()
+
+func _on_master_call_timer_timeout():
+	print("did this happen?")
+	call_seppo()
+	$CallLoop.start()
+
+
+func _on_call_loop_timeout():
+	print("call again")
+	call_seppo()
 
 
 func _on_audio_stream_player_finished():
-	$AudioStreamPlayer.play()
+	$MainMusic.play()
