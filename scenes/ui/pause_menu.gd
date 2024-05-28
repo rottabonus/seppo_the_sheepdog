8774f1befcbba663c765647ae6c7c75e55c97b62
Felix Hallenberg <feqwin@gmail.com>
2024-05-28 11:16:56 +0300

    Add some pausemenu-buttons

--- scenes/ui/pause_menu.gd
+++ scenes/ui/pause_menu.gd
@@ -6,5 +6,19 @@ func _ready():
 func _on_main_is_paused(next):
 	if next:
 		show()
+		$UI/Panel/VBoxContainer/ResumeButton.grab_focus()
 	else:
 		hide()
+
+func _on_resume_button_pressed():
+	hide()
+	get_tree().paused = false
+
+
+func _on_restart_button_pressed():
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
